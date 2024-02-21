f043cb0f147970ed830b22bf1ea4e66dbe1f4d4d
Danish Siddiqui <danbot555@gmail.com>
2024-02-21 16:36:18 -0500

    created projects page

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def index():
     return(render_template("index.html"))
 
+@app.route('/projects')
+def index():
+    return(render_template("projects.html"))
+
 
 app.run(
     host="0.0.0.0", port=5000
